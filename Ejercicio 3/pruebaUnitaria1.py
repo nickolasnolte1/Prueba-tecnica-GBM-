664c3159e6d5c1b680e252a03d83ee2be7dd8697
Nickolas Nolte <nickolasnolte@ufm.edu>
2022-09-26 23:58:27 -0600

    Ejercicio 3

--- Ejercicio 3/pruebaUnitaria1.py
+++ Ejercicio 3/pruebaUnitaria1.py
@@ -1,9 +1,24 @@
 import unittest
-import jumps 
-
-class TestJumpToX(unittest.TestCase):
-    def test_jump_to_x(self):
-        self.assertEqual(jump_to_x(10), 3)
-
+import jumps
+ 
+class TestMinJumps(unittest.TestCase):
+    def test_case_1(self):
+        x = 6
+        expected = 2
+        result = minJumps(x)
+        self.assertEqual(expected, result)
+ 
+    def test_case_2(self):
+        x = 10
+        expected = 3
+        result = minJumps(x)
+        self.assertEqual(expected, result)
+ 
+    def test_case_3(self):
+        x = 15
+        expected = 4
+        result = minJumps(x)
+        self.assertEqual(expected, result)
+ 
 if __name__ == '__main__':
     unittest.main()
