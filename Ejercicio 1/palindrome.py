a7d66965d736f14514002ae31fa9593963f78724
Nickolas Nolte <nickolasnolte@ufm.edu>
2022-09-28 18:14:59 -0600

    Update Ejercicio 1

--- Ejercicio 1/palindrome.py
+++ Ejercicio 1/palindrome.py
@@ -1,5 +1,19 @@
 def palindrome(palabra): 
-    return palabra == palabra[::-1] 
+    return palabra == palabra[::-1] #retorna el valor (T or F) 
 print(palindrome("radar")) 
 print(palindrome("carro")) 
 print(palindrome("arenera"))
+
+
+
+# Más palabras palíndromas para probar:
+# oso
+# ese
+# ojo
+# ana
+# somos
+# solos
+# salas
+# sometemos
+# sagas
+# seres
